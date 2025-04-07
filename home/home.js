18816c923992fc889b8e83d13733ad7bb3791309
antastur <antastur@gmail.com>
2025-04-07 10:30:10 +0200

    corregida politica cookies

--- home/home.js
+++ home/home.js
@@ -11,33 +11,51 @@ function showNextImage() {
 // Inicializa el carrusel mostrando solo la primera imagen y configura el temporizador
 document.addEventListener("DOMContentLoaded", () => {
 
-//Gestión del carrusel    
-const images = document.querySelectorAll(".carrusel img");
-images.forEach((img, index) => img.style.display = index === 0 ? "block" : "none"); // Muestra solo la primera imagen
-setInterval(showNextImage, 6000); // Cambia de imagen cada 6 segundos
-
-
-
-
-//Gestión de cookies
-
+//Para la gestión de cookies
 const cookiePopup = document.getElementById("cookieConsent");
+const cookieOverlay = document.getElementById("cookieOverlay");
 const acceptButton = document.getElementById("acceptCookies");
+const rejectButton = document.getElementById("rejectCookies");
+
+ // Verifica si el usuario ya aceptó o rechazó las cookies
+ const cookiesAccepted = sessionStorage.getItem("cookiesAccepted");
+
 
 // Verifica si el usuario ya aceptó las cookies
-if (!localStorage.getItem("cookiesAccepted")) {
+if (!cookiesAccepted) {
     cookiePopup.style.display = "flex"; // Muestra la ventana emergente
+    cookieOverlay.style.display = "block"; // Muestra el overlay
+}
+
+if (cookiesAccepted === "true") {
+    // Si las cookies fueron aceptadas, no muestra la ventana ni el overlay
+    cookiePopup.style.display = "none";
+    cookieOverlay.style.display = "none";
+} else {
+    // Si no hay decisión previa o si las cookies fueron rechazadas, muestra la ventana y el overlay
+    cookiePopup.style.display = "flex";
+    cookieOverlay.style.display = "block";
 }
 
 // Maneja el clic en el botón "Aceptar"
 acceptButton.addEventListener("click", () => {
-    localStorage.setItem("cookiesAccepted", "true"); // Guarda el consentimiento en localStorage
+    sessionStorage.setItem("cookiesAccepted", "true"); // Guarda el consentimiento en localStorage
     cookiePopup.style.display = "none"; // Oculta la ventana emergente
+    cookieOverlay.style.display = "none"; // Oculta el overlay
+});
+
+// Maneja el clic en el botón "Rechazar"
+rejectButton.addEventListener("click", () => {
+    sessionStorage.setItem("cookiesAccepted", "false"); // Guarda el rechazo en localStorage
+    cookiePopup.style.display = "flex"; // Oculta la ventana emergente
+    cookieOverlay.style.display = "block"; // Oculta el overlay
 });
 
 
-
-
+//Gestión del carrusel    
+const images = document.querySelectorAll(".carrusel img");
+images.forEach((img, index) => img.style.display = index === 0 ? "block" : "none"); // Muestra solo la primera imagen
+setInterval(showNextImage, 6000); // Cambia de imagen cada 6 segundos
 
 
 });
