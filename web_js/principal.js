3d8a225c03bbd6be668f1c5aa209887261b6a05b
antastur <antastur@gmail.com>
2025-04-21 14:54:52 +0200

    implementando mediaquerys

---
+++ web_js/principal.js
@@ -0,0 +1,4 @@
+document.getElementById("hamburger").addEventListener("click", function () {
+    const menuNav = document.getElementById("menuNav");
+    menuNav.classList.toggle("active"); // Alterna la clase "active" para mostrar/ocultar el menú
+});
